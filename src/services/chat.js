31781239cccec9a625b8d7c047b0953ffba9da96
aliroohan <aliroohan321@gmail.com>
2025-11-17 16:49:43 +0500

    fixed chat issue

--- src/services/chat.js
+++ src/services/chat.js
@@ -1,32 +1,17 @@
-const chatModel = require('../models/chat');
-const customerModel = require('../models/customer');
+const chatModel = require("../models/chat");
 
 exports.createChat = async ({ customerId, babysitterId, user = null }) => {
-    if (user.type === 'customer') {
-        if (user.availableChats > 0) {
-            const chat = new chatModel({ customerId, babysitterId });
-            await chat.save();
-            const customer = await customerModel.findById(customerId);
-            customer.availableChats--;
-            await customer.save();
-            return chat;
-        } else {
-            throw new Error('No available chats');
-        }
-    } else {
-        const chat = new chatModel({ customerId, babysitterId });
-        await chat.save();
-        return chat;
-    }
-}
+    const chat = new chatModel({ customerId, babysitterId });
+    await chat.save();
+    return chat;
+};
 
 exports.getChatsById = async (user) => {
-    if (user.type === 'customer') {
+    if (user.type === "customer") {
         const chats = await chatModel.find({ customerId: user.id });
         return chats;
     } else {
         const chats = await chatModel.find({ babysitterId: user.id });
         return chats;
     }
-}
-
+};
